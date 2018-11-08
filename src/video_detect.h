16ef296738824a1f520f5466d5ae22fcd236e42f
Anliec <nicolas.six@pau.fr>
2018-11-08 09:32:16 -0500

    fix semicolon

--- src/video_detect.h
+++ src/video_detect.h
@@ -3,6 +3,6 @@
 
 #include "image.h"
 
-void detect_in_video(char *cfgfile, char *weightfile, float thresh, const char *video_filename, char *classes_names_file, int classes_count, float hier, char *json_output_file)
+void detect_in_video(char *cfgfile, char *weightfile, float thresh, const char *video_filename, char *classes_names_file, int classes_count, float hier, char *json_output_file);
 
 #endif
