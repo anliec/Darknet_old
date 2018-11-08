a15d4b551a1230cdc8928e3ed21b94171a59d4b6
Anliec <nicolas.six@pau.fr>
2018-11-08 10:44:27 -0500

    fix compilation without openCV

--- src/video_detect.c
+++ src/video_detect.c
@@ -298,7 +298,8 @@ void detect_in_video(char *cfgfile, char *weightfile, float thresh, const char *
 }
 
 #else
-void demo(char *cfgfile, char *weightfile, float thresh, int cam_index, const char *filename, char **names, int classes, int delay, char *prefix, int avg_array, float hier, int w, int h, int frames, int fullscreen)
+void detect_in_video(char *cfgfile, char *weightfile, float thresh, const char *video_filename,
+        char *classes_names_file, int classes_count, float hier, char *json_output_file)
 {
     fprintf(stderr, "Demo needs OpenCV for webcam images.\n");
 }
