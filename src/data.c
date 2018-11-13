51e8660d2329cc8e567d5096b0ac6b25b17dc500
Anliec <nicolas.six@pau.fr>
2018-11-13 12:15:28 -0500

    display both images before wait key

--- src/data.c
+++ src/data.c
@@ -1087,8 +1087,8 @@ data load_data_detection(int n, char **paths, int m, int w, int h, int boxes, in
         fill_truth_detection(random_paths[i], boxes, d.y.vals[i], classes, flip, -dx/w, -dy/h, nw/w, nh/h);
 
         printf("orig: %dx%d\tcrop: %dx%d\n", orig.w, orig.h, sized.w, sized.h);
-        show_image(orig, "orig", 0);
-        show_image(sized, "crop", 0);
+        show_image(orig, "orig", 1);
+        show_image(sized, "crop", 1);
         cvWaitKey(0);
 
         free_image(orig);
